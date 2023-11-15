65471d407cc5f4a93c0c25d5afee294df05afab1
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-11-15 15:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let formsDataCollect = document.querySelector(".forms");
+forms.addEventListener("onsubmit", showData);
+function showData(e){
+	e.preventDefault();
+}
