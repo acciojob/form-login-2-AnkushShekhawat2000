354159d574f98fa5ab9d456eaf621758808b433e
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-11-29 16:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 //your JS code here. If required.
-let formsDataCollect = document.querySelector(".forms");
-formsDataCollect.addEventListener("submit", showData);
-function showData(e){
+   let formsDataCollect = document.querySelector(".forms");
+        formsDataCollect.addEventListener("submit", showData);
 
+        function showData(e) {
+            e.preventDefault();
 
-	e.preventDefault();
-	let data = {
-		name:document.querySelector(".firstName").value,
-	    lastName:document.querySelector(".lastName").value,
-     	phoneNumber:document.querySelector(".phoneNumber").value,
-		email:document.querySelector(".email").value,
-	
-		}
-	
-	
-	alert(data);
-}
+            let data = {
+                firstName: document.querySelector(".firstName").value,
+                lastName: document.querySelector(".lastName").value,
+                phoneNumber: document.querySelector(".phoneNumber").value,
+                email: document.querySelector(".email").value
+            };
+
+            alert(
+                "First Name: " + data.firstName + "\n" +
+                "Last Name: " + data.lastName + "\n" +
+                "Phone Number: " + data.phoneNumber + "\n" +
+                "Email ID: " + data.email
+            );
+        }
